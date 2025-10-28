09d0daf89684697a4b7735a2f4ada5791bb40b07
Reo05-code <t.asaishi05@gmail.com>
2025-10-28 13:40:53 +0900

    useState

--- react-basic/src/App.jsx
+++ react-basic/src/App.jsx
@@ -2,18 +2,22 @@ import './App.css'
 import { Header } from './components/Header';
 import { MainContent } from './components/MainContent';
 import { Footer } from './components/Footer';
+import { useState } from 'react';
 
 function App() {
   const siteTitle = 'Reactの基礎を学ぼう';
   const currentYear = new Date().getFullYear();
+  const [progress, setProgress] = useState(0);
 
   return (
     <>
       <Header siteTitle={siteTitle}/>
-      <MainContent />
+      <MainContent progress={progress} setProgress={setProgress}/>
       <Footer siteTitle={siteTitle} currentYear={currentYear}/>
     </>
   )
 }
+
+
 
 export default App
--- react-basic/src/components/Header.jsx
+++ react-basic/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 export function Header({ siteTitle = 'サイト名未設定' }) {
-
+const handleClick = () => {
+    alert('当社では、プログラミング学習者向けにReactを活用した実践的な学習教材を提供しています。Reactの基本から応用まで、手を動かしながら学べるコンテンツを通じて、確かなスキル習得を目指せます。');
+  };
 
   return (
     <header>
@@ -9,6 +11,7 @@ export function Header({ siteTitle = 'サイト名未設定' }) {
       <h1>{siteTitle}</h1>
       <ul>
         <li><a href="#">HOME</a></li>
+        <li><a href="#" onClick={handleClick}>会社概要</a></li>
       </ul>
       </nav>
     </header>
